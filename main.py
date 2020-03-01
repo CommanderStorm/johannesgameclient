937bb613a2a6f20a6f154a511fc8ee32b4ed69a9
frank <frank@elsinga.de>
2020-03-01 17:13:59 +0100

    added main code

---
+++ main.py
@@ -0,0 +1,17 @@
+import threading
+import socket
+import sys
+import handler
+
+socki = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socki.connect(("localhost", 3000))
+
+def socki_recv(s):
+    global socki
+    handler.hanle(s, socki).handle()
+
+
+while True:
+    s = str(socki.recv(1024), "utf8")
+    reci = threading.Thread(target=socki_recv, args=s)
+    reci.start()
