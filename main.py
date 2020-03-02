6c2baaa2a827aae42b8e8d9b549502b6cdd54b65
Kroko-fant <schrottsuperdeluxe@gmail.com>
2020-03-02 10:40:36 +0100

    IPs und ports

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ from typing import Dict
 import handler
 
 # Consts
-PORT = 3000
+
+GAMEPORT = 1337
+RESULTPORT = 1338
+SCOREPORT = 1339
 IP_ADRESS = "10.183.83.127"
+NAME = "Python"
 
 # Var
-socki = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socki.connect((IP_ADRESS, PORT))
+gamesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+gamesocket.connect((IP_ADRESS, GAMEPORT))
 opponents: Dict[str, handler.Handle] = {}
 
 while True:
 
-    s = str(socki.recv(1024), "utf8")
+    s = str(gamesocket.recv(1024), "utf8")
     signature, s = s.split("ä")
 
     if signature not in opponents.keys():
-        opponents[signature] = handler.Handle(socki)
+        opponents[signature] = handler.Handle(gamesocket)
     threading.Thread(target=opponents[signature].handle, args=s).start()
