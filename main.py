58a8bce17af5707112544d88a1495d07082cd406
Kroko-fant <schrottsuperdeluxe@gmail.com>
2020-03-02 11:07:28 +0100

    connect try

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import socket
-import threading
-from typing import Dict
-
-import handler
-
+import sys
 # Consts
-
 GAMEPORT = 1337
 RESULTPORT = 1338
 SCOREPORT = 1339
@@ -15,13 +10,10 @@ NAME = "Python"
 # Var
 gamesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 gamesocket.connect((IP_ADRESS, GAMEPORT))
-opponents: Dict[str, handler.Handle] = {}
-
-while True:
-
-    s = str(gamesocket.recv(1024), "utf8")
-    signature, s = s.split(";")
-
-    if signature not in opponents.keys():
-        opponents[signature] = handler.Handle(gamesocket)
-    threading.Thread(target=opponents[signature].handle, args=s).start()
+gamesocket.send(bytes(NAME, "utf8") + b'\r\n')
+first = str(gamesocket.recv(1024), "utf8")
+if first == "Welcome":
+    print("Connected")
+else:
+    print("Something went wrong")
+    sys.exit(0)
