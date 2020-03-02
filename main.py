4ad2c745829d09e721b14aefde5fa3746782abaf
Kroko-fant <schrottsuperdeluxe@gmail.com>
2020-03-02 11:17:30 +0100

    connect try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import time as t
+
 # Consts
 GAMEPORT = 1337
 RESULTPORT = 1338
@@ -10,10 +12,10 @@ NAME = "Python"
 # Var
 gamesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 gamesocket.connect((IP_ADRESS, GAMEPORT))
-gamesocket.send(bytes(NAME, "utf8") + b'\r\n')
-first = str(gamesocket.recv(1024), "utf8")
-if first == "Welcome":
-    print("Connected")
-else:
-    print("Something went wrong")
-    sys.exit(0)
+gamesocket.send(bytes(NAME, "utf8") + b'\n')
+while True:
+    first = str(gamesocket.recv(1024), "utf8")
+    if first == "Welcome\n":
+        print("Connected")
+    elif first == "Ping\n":
+        gamesocket.send(b'PONG')
